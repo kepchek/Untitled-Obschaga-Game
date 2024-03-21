58b0685889315e2b9ba724e9e6b8c58300e8c889
Kepchek <112941559+kepchek@users.noreply.github.com>
2024-03-21 23:33:26 +0300

    Add 2d engine

---
+++ Jam6/Assets/Scripts/Test.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+
+public class Test : MonoBehaviour
+{
+    public void Clickeds()
+    {
+        Debug.Log("Олег лохушка");
+    }
+}
