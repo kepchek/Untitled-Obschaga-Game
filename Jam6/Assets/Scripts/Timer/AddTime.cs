ef330b7f6b87e584f3a3f5081bc79ce6afc2a33a
Skrat <146776521+skrat1k@users.noreply.github.com>
2024-03-21 22:02:12 +0300

    add cube pls

--- Jam6/Assets/Scripts/Timer/AddTime.cs
+++ Jam6/Assets/Scripts/Timer/AddTime.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AddTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.tag == "Player")
+        {
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
