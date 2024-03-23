820d435c63d4ef9f260e107b5443a0eeba86d393
Kepchek <112941559+kepchek@users.noreply.github.com>
2024-03-24 00:48:53 +0300

    Появился в зоне чёрный сталкер

--- Jam6/Assets/Scripts/Minigames/MinigamesSystem/MinigameSystem.cs
+++ Jam6/Assets/Scripts/Minigames/MinigamesSystem/MinigameSystem.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class MinigameSystem : MonoBehaviour
 {
@@ -9,15 +11,20 @@ public class MinigameSystem : MonoBehaviour
     public GameObject MinigameTrigger2;
     public GameObject MinigameTrigger3;
     public GameObject MinigameTrigger4;
+    int randomMGvalue; //Случайный выбор числа, для выбора миниигры
+
 
     private void Awake() 
     {
+        randomMGvalue = UnityEngine.Random.Range(0, 22);
+        Debug.Log(randomMGvalue);
         MinigameTrigger0 = GameObject.Find("MinigameTrigger0");
         MinigameTrigger1 = GameObject.Find("MinigameTrigger1");
         MinigameTrigger2 = GameObject.Find("MinigameTrigger2");
         MinigameTrigger3 = GameObject.Find("MinigameTrigger3");
         MinigameTrigger4 = GameObject.Find("MinigameTrigger4");
     }
+    
 
  //МЕТОД ДЛЯ ТЕСТА
     private void Update() {
@@ -26,9 +33,10 @@ public class MinigameSystem : MonoBehaviour
             StartMinigame0();
             Debug.Log("Minigame ready!");
         }
+        
     }
 
-    void StartMinigame0()
+    public void StartMinigame0()
     {
         MinigameTrigger0.GetComponent<InteractTrigger>().TriggerIsEnabled = true;
     }
@@ -48,8 +56,47 @@ public class MinigameSystem : MonoBehaviour
     {
         MinigameTrigger4.GetComponent<InteractTrigger>().TriggerIsEnabled = true;
     }
+
+    void RandomMethodInvoke()
+    {
+        /*
+        Ценность игр
+        Кликер - 6
+        Роутер - 5
+        Провода - 4
+        Носки - 4
+        Водка - 2
+
+        */
+        if((randomMGvalue > 0) & (randomMGvalue <= 6)) //Выбор миниигры посредством случайного числа и ценности миниигры
+        {
+            //кликер
+        }
+        else if((randomMGvalue > 6) & (randomMGvalue <= 11))
+        {
+            //роутер
+        }
+        else if((randomMGvalue > 11) & (randomMGvalue <= 15))
+        {
+            //провода
+        }
+        else if((randomMGvalue > 15) & (randomMGvalue <= 19))
+        {
+            //носки
+        }
+        else if((randomMGvalue > 19) & (randomMGvalue <= 21))
+        {
+            //водка
+        }
+
+    }
+
+    /*
     public void ExitMinigame0()
     {
         MinigameTrigger0.GetComponent<InteractTrigger>().TriggerIsEnabled = false;
     }
+    */
+
+
 }
